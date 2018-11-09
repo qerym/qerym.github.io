763003a1122a2b3acb1b02e859b92d8c26f92858
qerym <44892197+qerym@users.noreply.github.com>
2018-11-09 14:32:00 +0600

    hdhd

---
+++ functions/functions.php
@@ -0,0 +1,12 @@
+﻿<?php
+$mysqli = FALSE;
+function connectdb () {
+	global $mysqli;
+	$mysqli = new mysqli("localhost", "root", "", "bugrovabase");
+	$mysqli ->query("SET NAMES 'utf-8' ");
+}
+function closedb () {
+global $mysqli;
+$mysqli->close ();	
+}
+?>
